17c82e3c37d09c2c52bff064675b006c9cacf378
kainmaligno <s.e.jugr@gmail.com>
2018-11-22 13:47:51 -0600

    test

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-$(window).on("load", () =>{
-
-    $('#btn').on("click", () => {
-    
-       $.post('https://execution-use.ci360.sas.com/t/e/df2b9cb68c00010112fb657b', (data,status)=> {
-        console.log('llegas')
-        alert("data" + data + "status" + status)
-       })
-      
+    $('#btn').on("click", (e) => {
+    // e.preventDefault()
+         $.post('https://execution-use.ci360.sas.com/t/e/df2b9cb68c00010112fb657b',(data)=>{alert('funciona'+data)})
+      console.log('estas en boton de click')
     })
    
-})
