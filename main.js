6fbd038f2c04bdb0fa24029b987bb98c2f956a9c
kainmaligno <s.e.jugr@gmail.com>
2018-11-22 11:19:48 -0600

    test

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(window).on("load", () =>{
 
-    // $('#btn').on("click", () => {
-    //    alert("si funciona el boton")
-       
-    // })
+    $('#btn').on("click", () => {
+       alert("si funciona el boton")
+       $.post('https://execution-use.ci360.sas.com/t/e/df2b9cb68c00010112fb657b')
+    })
    
 })
